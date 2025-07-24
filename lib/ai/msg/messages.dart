b32cebf4b7bf3b745fb4c24b8d16967a941c21cd
Michele Mazzei <michele.mazzei@persociv.difesa.it>
2025-07-24 20:38:08 +0200

    la classe Messages deve essere sealed

--- lib/ai/msg/messages.dart
+++ lib/ai/msg/messages.dart
@@ -3,7 +3,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'messages.freezed.dart';
 
 @freezed
-abstract class Messages with _$Messages {
+sealed class Messages with _$Messages {
   /// Ordina all'agente di tornare alla sua posizione originaria.
   const factory Messages.goHome() = GoHome;
 
